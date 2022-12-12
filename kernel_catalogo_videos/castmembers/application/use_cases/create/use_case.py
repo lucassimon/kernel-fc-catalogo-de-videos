e40a0f9bee307c561aced9595ad1f5e667c3daec
Lucas <lucassrod@gmail.com>
2022-12-12 19:57:48 -0300

    Improve create castmember

--- kernel_catalogo_videos/castmembers/application/use_cases/create/use_case.py
+++ kernel_catalogo_videos/castmembers/application/use_cases/create/use_case.py
@@ -8,6 +8,7 @@ from dataclasses import asdict
 # Apps
 from kernel_catalogo_videos.core.application.use_case import UseCase
 from kernel_catalogo_videos.castmembers.domain.entities import CastMember
+from kernel_catalogo_videos.castmembers.domain.factories import CastMemberFactory
 from kernel_catalogo_videos.castmembers.domain.repositories import CastMemberRepository
 from kernel_catalogo_videos.castmembers.application.use_cases.dto import CastMemberOutputMapper
 from kernel_catalogo_videos.castmembers.application.use_cases.create.input import CreateCastMemberInput
@@ -25,20 +26,22 @@ class CreateGenreUseCase(UseCase[CreateCastMemberInput, CreateCastMemberOutput])
         self.repo = repo
         self.logger = logger
 
+    def save(self, entity: CastMember):
+        self.repo.insert(entity)
+        if self.logger:
+            self.logger.info("create.castmember.usecase", message="Entity saved")
+
     def execute(self, input_params: CreateCastMemberInput) -> CreateCastMemberOutput:
         if self.logger:
             self.logger.info("create.castmember.usecase", message="Initial Payload", input_params=asdict(input_params))
 
-        castmember = CastMember(name=input_params.name, kind=input_params.kind, status=input_params.status)
+        castmember_entity = CastMemberFactory(logger=self.logger).create(
+            name=input_params.name, kind=input_params.kind, status=input_params.status
+        )
 
-        if self.logger:
-            self.logger.info("create.castmember.usecase", message="Entity created", castmember=castmember.to_dict())
+        self.save(castmember_entity)
 
-        self.repo.insert(castmember)
-        if self.logger:
-            self.logger.info("create.castmember.usecase", message="Entity saved")
-
-        return self.__to_output(castmember=castmember)
+        return self.__to_output(castmember=castmember_entity)
 
     def __to_output(self, castmember: CastMember):
         return CastMemberOutputMapper.to_output(klass=CreateCastMemberOutput, castmember=castmember)
